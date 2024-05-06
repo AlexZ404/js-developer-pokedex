a8f128d3301fbd3ca7642b46204d8add818f5958
AlexZ404 <alexandremth1@gmail.com>
2024-05-06 17:41:58 -0300

    Atualização 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,18 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+    <li class="pokemon ${pokemon.type}">
+    <a class="pokemon-link" href="/pokemon-details.html?number=${pokemon.number}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+        </div>
+    </a>
+</li>
     `
 }
 
@@ -44,4 +43,10 @@ loadMoreButton.addEventListener('click', () => {
     } else {
         loadPokemonItens(offset, limit)
     }
+})
+
+const icone = document.getElementsByClassName('pokemons')
+
+icone.addEventListener("click", () => {
+    window.location.href = `./detail.html?id=${pokemon.number}`;
 })
